06c5b24ad1cbaccbc7087777d0cf2dde37441029
JoaoPedro0803 <joaopedrocvaz@gmail.com>
2024-08-06 10:05:18 -0300

    Alterações back de favoritar e add video para ver mais tarde

--- src/controller/savedVideosController.py
+++ src/controller/savedVideosController.py
@@ -16,18 +16,15 @@ def add_to_watch_later(watch_later: savedVideosSchema.WatchLaterCreate, db: Sess
 
 @WatchLater.delete("/{video_id}")
 def remove_from_watch_later(video_id: str, user_id: str = Query(...), db: Session = Depends(get_db)):
-   print(f"Attempting to remove video_id={video_id} for user_id={user_id}")
-   user_id = user_id.strip()  # Certifique-se de que o `user_id` não contém espaços ou quebras de linha
-   video_id = video_id.strip()  # Certifique-se de que o `video_id` não contém espaços ou quebras de linha
+   user_id = user_id.strip()  
+   video_id = video_id.strip()
    savedVideosRepository.remove_watch_later(db=db, video_id=video_id, user_id=user_id)
    return {"message": "Removed from watch later list"}
 
 
 @WatchLater.get("/status/{video_id}")
 def check_watch_later(video_id: str, user_id: str = Query(...), db: Session = Depends(get_db)):
-   print(f"Checking watch later status for video_id={video_id}, user_id={user_id}")
    status = savedVideosRepository.check_watch_later_status(db=db, video_id=video_id, user_id=user_id)
-   print(f"Status for video_id={video_id}, user_id={user_id} is {status}")
    return {"status": status}
 
 # início das requisições do favorite
