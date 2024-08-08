27dae8a2e9031d878e7cb75ce561609fabdf3549
gabrielhrlima <ghrl2003@gmail.com>
2024-08-08 20:28:07 -0300

    Modifica funcao para alterar o historico

--- src/controller/recordController.py
+++ src/controller/recordController.py
@@ -11,7 +11,12 @@ Record = APIRouter(
 
 @Record.post("/")
 def add_to_record(record: recordSchema.RecordCreate, db: Session = Depends(get_db)):
-   return recordRepository.create_record(db=db, record=record)
+   record_user = recordSchema.RecordGet(user_id =record.user_id)
+   videos = recordRepository.get_record(db = db , record = record_user)
+   if videos:
+      return recordRepository.create_record(db=db, record=record, is_creat= False)
+
+   return recordRepository.create_record(db=db, record=record, is_creat= True)
 
 @Record.get("/get_record")
 def check_record(user_id: str =Query(...) , db: Session = Depends(get_db)):
--- src/repository/recordRepository.py
+++ src/repository/recordRepository.py
@@ -4,12 +4,25 @@ from domain import recordSchema
 from model import recordModel
 from fastapi import HTTPException 
 
-def create_record(db: Session, record: recordSchema.RecordCreate):
-   db_record = recordModel.Record(
-       user_id=record.user_id.strip(),
-       videos=record.videos
-   )
-   db.add(db_record)
+def create_record(db: Session, record: recordSchema.RecordCreate, is_creat):
+   if is_creat == True:
+    db_record = recordModel.Record(
+        user_id=record.user_id.strip(),
+        videos=record.videos
+    )
+    db.add(db_record)
+   else:
+        db_record = db.query(recordModel.Record).filter(recordModel.Record.user_id == record.user_id).first()
+
+        id_video = list(record.videos.keys())[0]
+        timestamp_video = list(record.videos.values())[0]
+
+        video_append = db_record.videos.copy()
+        video_append[id_video] = timestamp_video
+        print(video_append)
+        
+        db_record.videos = video_append
+
    db.commit()
    db.refresh(db_record)
    print(f"Created record: user_id={db_record.user_id}")
