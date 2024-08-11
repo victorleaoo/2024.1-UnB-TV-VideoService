ccfcff8d6378bcc8ade3710a265643d8e7b0cad7
Victor Leão <victor.pessoal1203@gmail.com>
2024-08-10 23:28:16 -0300

    Create recordController.py

---
+++ src/controller/recordController.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, HTTPException, Depends, Query
+from sqlalchemy.orm import Session
+from domain import recordSchema
+from database import get_db
+from repository import recordRepository
+from starlette.responses import JSONResponse
+
+Record = APIRouter(
+ prefix="/record"
+)
+
+@Record.post("/")
+def add_to_record(record: recordSchema.RecordCreate, db: Session = Depends(get_db)):
+   record_user = recordSchema.RecordGet(user_id =record.user_id)
+   videos = recordRepository.get_record(db = db, record = record_user)
+   if videos:
+      return recordRepository.create_record(db=db, record=record, is_create= False)
+
+   return recordRepository.create_record(db=db, record=record, is_create= True)
+
+@Record.get("/get_record")
+def check_record(user_id: str =Query(...) , db: Session = Depends(get_db)):
+   record = recordSchema.RecordGet(user_id =user_id)
+   videos = recordRepository.get_record(db=db, record=record)
+   return {"videos": videos}
