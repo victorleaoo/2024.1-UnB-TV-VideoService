5e3283945ac926bd403c9cba7fadb052e76ad083
Benjamim Lacerda <benjamim.lacerda16@gmail.com>
2024-08-21 19:18:27 -0300

    Ajuste no diretorio teste do modelo

---
+++ recomendation_model/model_pairwise_todos.py
@@ -0,0 +1,60 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import pairwise_distances
+import pandas as pd
+import string
+
+from utils.constants import PORTUGUESE_STOP_WORDS
+
+from utils.model import Catalog
+
+from utils.get_videos import find_all_videos, videos_to_dataframe, add_title_to_file
+from utils.categorize_videos import categorize_videos
+
+
+# Calcula similaridade dos vídeos a partir da distância dos cossenos (cosine distance)
+def calculate_similarity(df):
+    tfidf = TfidfVectorizer(stop_words=PORTUGUESE_STOP_WORDS)
+    tfidf_matrix = tfidf.fit_transform(df['Categoria'] + ' ' + df['Título'] + ' ' + df['Descrição'])
+    distance = pairwise_distances(tfidf_matrix, metric='manhattan')
+    return distance
+
+
+# Obtém uma lista de vídeos recomendados a partir de um título
+def get_recommendations(title, distance, df):
+    indices = pd.Series(df.index, index=df['Título']).drop_duplicates()
+
+    sim_scores = sorted(list(enumerate(distance[indices[title]])), key=lambda x: x[1], reverse=True)[
+                 1:11]  # Pega os 5 melhores
+
+    video_indices = [i[0] for i in sim_scores]
+    return df.iloc[video_indices]
+
+
+if __name__ == "__main__":
+    catalog = Catalog()
+    videos = find_all_videos()
+
+    if videos:
+        # Categorização dos vídeos
+        categorize_videos(videos, catalog)
+
+        # Cria o DataFrame com as categorias
+        df = videos_to_dataframe(videos)
+
+        # Calcula a similaridade
+        distance = calculate_similarity(df)
+
+        # Obtém recomendações
+        ##### MODIFICAR NOME DO TITULO #####
+        title = 'IAgora Entrevista: IA, Ciência de Dados e perspectivas'  # Substitua pelo título desejado
+        title = title.lower().translate(str.maketrans('', '', string.punctuation))
+        recommendations = get_recommendations(title, distance, df)
+
+        ##### MODIFICAR NOME DO ARQUIVO #####
+        file_path = './model_cosine_sim_results/3PAIRWISE_MANHATTAN_TODOS.csv'
+        recommendations.to_csv(file_path)
+        add_title_to_file(file_path, title)
+
+        print("Recomendações:\n", recommendations)
+    else:
+        print("Nenhum vídeo encontrado.")
