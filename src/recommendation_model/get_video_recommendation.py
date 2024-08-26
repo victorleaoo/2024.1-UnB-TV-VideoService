5f2f563113397b3f2a82374e83d5912c4331fccd
Victor Leão <victor.pessoal1203@gmail.com>
2024-08-25 21:12:34 -0300

    correção para nao usar pandas

--- src/recommendation_model/get_video_recommendation.py
+++ src/recommendation_model/get_video_recommendation.py
@@ -1,17 +1,31 @@
-import pandas as pd
+import csv
 import pickle
 
+# Carrega o arquivo CSV em uma lista de dicionários, ignorando a primeira coluna
+def load_csv_as_dict(file_path):
+    with open(file_path, mode='r') as file:
+        reader = csv.DictReader(file)
+        return [{'ID': row['ID'], 'Título': row['Título'], 'Descrição': row['Descrição'], 'Categoria': row['Categoria']} for row in reader]
+
+# Cria um dicionário indexado por ID
+def create_indexed_dict(data):
+    return {item['ID']: index for index, item in enumerate(data)}
+
 # Obtém uma lista de vídeos recomendados a partir de um ID
-def get_recommendations(id):
+def get_recommendations(video_id):
     with open('/app/src/recommendation_model/cosine_similarity.pkl', 'rb') as f:
         cosine_sim = pickle.load(f)
 
-    df = pd.read_csv('/app/src/recommendation_model/df_videos.csv')
+    data = load_csv_as_dict('/app/src/recommendation_model/df_videos.csv')
+    id_index = create_indexed_dict(data)
 
-    indices = pd.Series(df.index, index=df['ID']).drop_duplicates()
+    # Verifica se o ID está presente no dicionário
+    if str(video_id) not in id_index:
+        raise ValueError(f"ID {video_id} não encontrado no dicionário.")
 
-    sim_scores = sorted(list(enumerate(cosine_sim[indices[id]])), key=lambda x: x[1], reverse=True)[
-                 1:8]  # Pega os 7 melhores
+    # Encontra os índices dos vídeos similares
+    sim_scores = sorted(list(enumerate(cosine_sim[id_index[str(video_id)]])), key=lambda x: x[1], reverse=True)[1:8]
 
+    # Retorna uma lista de IDs recomendados
     video_indices = [i[0] for i in sim_scores]
-    return list(df.iloc[video_indices][['ID']]['ID'])
+    return [int(data[i]['ID']) for i in video_indices]
